6b5ec5fc8b82b1fc73ee7f7d4607c0296af84b14
James Carty <jacarty@cisco.com>
2017-07-04 12:36:21 +0100

    week 4, log file to dictionary example

---
+++ exercise #4: logtodict/logtodict.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""For each line of the log file create a dictionary and, in turn add that to a list."""
+
+import sys
+import re
+
+listofdict = []
+
+ip = re.compile('\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}')
+time = re.compile('\d{1,2}/\w{3}/\S+ [+]\d{4}')
+web_request = re.compile('"\w{3} /(\S+)? \w{4}/\d.\d')
+
+
+def logtodict(textfile):
+
+    for each_line in open(textfile):
+
+        dic = {
+            'ip_address': ip.search(each_line).group(),
+            'timestamp': time.search(each_line).group(),
+            'request': web_request.search(each_line).group()
+        }
+
+        listofdict.append(dic)
+
+    print listofdict
+
+if __name__ == '__main__':
+    logtodict(sys.argv[1])
